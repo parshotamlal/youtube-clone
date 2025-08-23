04bc63fdb1e6a8987012dfcc50578e90c64363ec
kanav789 <kanavrana91@gmail.com>
2025-08-23 10:46:52 +0530

    added the addpost router

--- youtubeBackend/routes/postRoutes.js
+++ youtubeBackend/routes/postRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { authenticateToken } from "../middleware/auth.js";
-import { AddPost, getPosts } from "../controllers/PostController.js";
+import {
+  AddPost,
+  getPosts,
+  updatePost,
+} from "../controllers/PostController.js";
 
 const router = Router();
 
 router.post("/addpost", authenticateToken, AddPost);
 router.get("/getpost", authenticateToken, getPosts);
+router.post("/updatepost/:id", authenticateToken, updatePost);
 export default router;
