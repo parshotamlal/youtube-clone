44cd40bbc9fd98c35771f88047f8f7b65f32c50e
kanav789 <kanavrana91@gmail.com>
2025-08-25 10:18:04 +0530

    fetch all the details of channel

--- frontent/src/pages/Channel.jsx
+++ frontent/src/pages/Channel.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 function Channel() {
   const [user, setUser] = useState(null);
+  const [userdetail, setUserDetails] = useState(false);
   const [selectedVideo, setSelectedVideo] = useState(null);
 
   useEffect(() => {
@@ -23,7 +24,25 @@ function Channel() {
       }
     };
 
+    const fetchuser = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}user/profile`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("youtubetoken")}`,
+            },
+          }
+        );
+
+        setUserDetails(response.data?.data?.user);
+      } catch (err) {
+        console.error("Error fetching channel data:", err);
+      }
+    };
+
     fetchData();
+    fetchuser();
   }, []);
 
   return (
@@ -32,18 +51,20 @@ function Channel() {
       <div className="h-40 md:h-60 bg-gradient-to-r from-gray-900 to-gray-700 relative">
         <div className="absolute bottom-0 left-6 transform translate-y-1/2">
           <div className="w-24 h-24 md:w-32 md:h-32 rounded-full bg-gray-300 border-4 border-white flex items-center justify-center text-xl font-bold">
-            {user?.name ? user.name[0].toUpperCase() : "U"}
+            {userdetail?.username ? userdetail.username.toUpperCase() : "U"}
           </div>
         </div>
       </div>
 
       {/* User Info */}
-      {user && (
+      {userdetail && (
         <div className="mt-16 px-6 border-b pb-6">
           <h1 className="text-2xl md:text-3xl font-bold">
-            {user?.name || "User Name"}
+            {userdetail?.username || "User Name"}
           </h1>
-          <p className="text-gray-600">{user?.email || "No email provided"}</p>
+          <p className="text-gray-600">
+            {userdetail?.email || "No email provided"}
+          </p>
         </div>
       )}
 
